f44896509d17dfefd3409215be298c6984380687
Gersoncs107 <gerson.silva107@gmail.com>
2026-09-19 19:43:33 -0300

    flight-diaries-frontend-update

--- src/services/diaryService.ts
+++ src/services/diaryService.ts
@@ -1,11 +1,10 @@
-import axios from "axios";
-import type { DiaryEntry } from "../types";
+import axios from 'axios';
+import type { DiaryEntry } from '../types';
 
-const baseUrl = "http://localhost:3001/api/diaries";
+const baseUrl = 'http://localhost:3000/api/diaries';
 
-const getAll = async (): Promise<DiaryEntry[]> => {
-  const response = await axios.get<DiaryEntry[]>(baseUrl);
-  return response.data;
+const getAll = () => {
+  return axios.get<DiaryEntry[]>(baseUrl).then(response => response.data);
 };
 
 export default { getAll };
