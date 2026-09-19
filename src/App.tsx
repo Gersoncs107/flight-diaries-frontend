0ff138cb8cfbb8e7e2c691ef47588aeb33bbf4dc
Gersoncs107 <gerson.silva107@gmail.com>
2026-09-19 19:45:17 -0300

    flight-diaries-frontend-update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import { useState, useEffect } from 'react'
+import { DiaryEntry } from './types'
+import diaryService from './services/diaryService'
 
 function App() {
-  
+  const [diaries, setDiaries] = useState<DiaryEntry[]>([])
+
+  useEffect(() => {
+    diaryService.getAll().then(data => {
+      setDiaries(data)
+    })
+  }, [])
 
   return (
-    <>
-      
-    </>
+    <div>
+      <h2>Flight Diaries</h2>
+      {diaries.map(diary => (
+        <div key={diary.id}>
+          <h3>{diary.date}</h3>
+          <p>visibility: {diary.visibility}</p>
+          <p>weather: {diary.weather}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
