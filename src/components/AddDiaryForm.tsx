7db7585371724e54c2b67dcacb032274da100fa0
Gersoncs107 <gerson.silva107@gmail.com>
2026-09-19 22:27:05 -0300

    flight-diaries-frontend-update

--- src/components/AddDiaryForm.tsx
+++ src/components/AddDiaryForm.tsx
@@ -4,3 +4,10 @@ import type { DiaryFormValues } from "../types";
 interface AddDiaryFormProps {
   onSubmit: (values: DiaryFormValues) => void;
 }
+
+const AddDiaryForm = ({ onSubmit }: AddDiaryFormProps) => {
+  const [date, setDate] = useState("");
+  const [weather, setWeather] = useState("");
+  const [visibility, setVisibility] = useState("");
+  const [comment, setComment] = useState("");
+  
